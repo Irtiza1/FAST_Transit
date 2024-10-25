8205f9f3bc4906f04fbf9e506337857e4d00eea9
Irtiza1 <irtiza.s2918@gmail.com>
2024-10-25 10:59:19 +0500

    Role based Access Successfully implemented

--- backend/src/middleware/AuthorizeRoleMiddleware.js
+++ backend/src/middleware/AuthorizeRoleMiddleware.js
@@ -1,27 +1,27 @@
 import jwt from "jsonwebtoken"
 
-export const authorizeRole= (...allowedRoles) =>{
-    return (req,res, next)=>{
-        const token= req.headers.authorization?.split(' ')[1]
+export const authorizeRole = (...allowedRoles) => {
+    return (req, res, next) => {
+        const token = req.headers.authorization?.split(' ')[1];
 
-        if(!token){
-            res.status(403).json({message: 'Access denied. No token provided'})
+        if (!token) {
+            return res.status(403).json({ message: 'Access denied. No token provided' });
         }
-
-        jwt.verify(token,process.env.JWT_SECRET,(err,decoded) =>{
-            if(err){
-                return res.status(403).json({message: 'Invalid token'})
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(403).json({ message: 'Invalid token' });
+            }
+            console.log(decoded);
+            // Fix the role check
+            if (!allowedRoles.includes(decoded.Role)) {
+                return res.status(403).json({ message: 'Access denied. Insufficient permissions' });
             }
 
-            if(!allowedRoles.includes(decoded.role)){
-                return res.status(403).json({message: 'Access denied. Insufficient permissions'})
-            }
+            req.user = decoded;
+            next();
+        });
+    };
+};
 
-            req.user= decoded;
-            next()
-        })
-    }
-
-}
 
 // export default authorizeRole :not using as i am importing wiht curly brackts
